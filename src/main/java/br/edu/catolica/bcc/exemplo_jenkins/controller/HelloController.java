fa76c95f26bec81c824307a20ffba12e8fee7d2e
lucciano01 <lucciano01@hotmail.com>
2025-04-16 21:49:52 -0300

    testando integracao jenkins&& git push

--- src/main/java/br/edu/catolica/bcc/exemplo_jenkins/controller/HelloController.java
+++ src/main/java/br/edu/catolica/bcc/exemplo_jenkins/controller/HelloController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloController {
 
+    //metodo para buscar nome de usuario
     @GetMapping("/{name}")
     public String helloUser(@PathVariable("name") String name){
         return "Seja bem vindo " + name;
